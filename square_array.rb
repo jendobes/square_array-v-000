47b6028afcd9d188316d2bcfbe68ffe10df03104
Jennifer Dobak <jdobak1@gmail.com>
2018-01-10 18:20:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,3 @@ def square_array(array)
     array.each { |number| new_array << number ** 2 }
     new_array
 end
-array = [1,2,3]
-def square_array(array)
-    array.collect { |number| number ** 2 }
-end
